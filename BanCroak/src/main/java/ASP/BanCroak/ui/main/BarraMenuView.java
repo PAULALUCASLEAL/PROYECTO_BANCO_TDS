2ac2a068865ec3f6aef5b0e29c5069b706a5956e
Aarón <a.lasherasguillen@um.es>
2026-02-13 11:56:29 +0100

    BarraMenu añadida

---
+++ BanCroak/src/main/java/ASP/BanCroak/ui/main/BarraMenuView.java
@@ -0,0 +1,41 @@
+package ASP.BanCroak.ui.main;
+
+import ASP.BanCroak.ui.app.SceneManager;
+import javafx.application.Platform;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+
+public class BarraMenuView extends MenuBar {
+	public BarraMenuView(SceneManager sm) {
+        Menu menuMenu = new Menu("Menu");
+        Menu menuVer = new Menu("Visualizar...");
+        Menu menuNotificaciones = new Menu("Notificaciones");
+        
+        MenuItem itemCrearGasto = new MenuItem("Crear gasto");
+        MenuItem itemCrearCuentaCompartida = new MenuItem("Crear cuenta compartida");
+        MenuItem itemSalir = new MenuItem("Salir");
+        MenuItem itemTabla = new MenuItem("Tabla");
+        MenuItem itemBarras = new MenuItem("Barras");
+        MenuItem itemCirculares = new MenuItem("Circulares");
+        MenuItem itemCalendario = new MenuItem("Calendario");
+        MenuItem itemCrearNotificacion = new MenuItem("Crear notificación");
+        MenuItem itemHistorial = new MenuItem("Historial de notificaciones");
+        
+        
+
+        /*itemCrearGasto.setOnAction(e -> sm.showVentanaCrearGasto());
+        itemCrearCuentaCompartida.setOnAction(e -> sm.showVentanaCuentaCompartida());
+        itemCrearNotificacion.setOnAction(e -> sm.showVentanaCrearNotificaciones());
+        itemTabla.setOnAction(e -> sm.showTabla());
+        itemHistorial.setOnAction(e -> sm.showVentanaHistorialNotificaciones());*/
+        itemSalir.setOnAction(e -> Platform.exit());
+
+        menuMenu.getItems().addAll(itemCrearGasto, new SeparatorMenuItem(), itemCrearCuentaCompartida, new SeparatorMenuItem(),itemSalir);
+        menuVer.getItems().addAll(itemTabla,new SeparatorMenuItem(), itemBarras,new SeparatorMenuItem(), itemCirculares, new SeparatorMenuItem(), itemCalendario);
+        menuNotificaciones.getItems().addAll(itemCrearNotificacion, new SeparatorMenuItem(),itemHistorial);
+
+        this.getMenus().addAll(menuMenu, menuVer,menuNotificaciones);
+    }
+}
--- BanCroak/src/main/java/ASP/BanCroak/ui/main/MainView.java
+++ BanCroak/src/main/java/ASP/BanCroak/ui/main/MainView.java
@@ -2,14 +2,10 @@ package ASP.BanCroak.ui.main;
 
 
 import ASP.BanCroak.ui.app.SceneManager;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
 
